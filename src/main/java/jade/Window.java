47aba446d9fcd924ac054fbc91ef13695aeefd47
kadawkalet <kongm.lim@gmail.com>
2021-12-14 09:17:53 +1100

    Scene Mgt fix

--- src/main/java/jade/Window.java
+++ src/main/java/jade/Window.java
@@ -4,15 +4,10 @@ import util.Time;
 import org.lwjgl.*;
 import org.lwjgl.glfw.*;
 import org.lwjgl.opengl.*;
-import org.lwjgl.system.*;
-
-
-import java.nio.*;
 
 import static org.lwjgl.glfw.Callbacks.*;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.system.MemoryStack.*;
 import static org.lwjgl.system.MemoryUtil.*;
 
 public class Window {
@@ -40,11 +35,12 @@ public class Window {
         switch (newScene) {
             case 0:
                 currentScene = new LevelEditorScene();
-                //currentScene.init();
+                currentScene.init();
                 break;
 
             case 1:
                 currentScene = new LevelScene();
+                currentScene.init();
                 break;
             default:
                 assert false: "Unknown scene '" + newScene + "'";
